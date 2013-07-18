8336838b8db3be84ccea607c886d7880a0e999d7
David Corking <dmc-source@dcorking.com>
2013-07-18 15:01:01 +0100

    Trying to get minimum code to get line 229 of org controller spec to pass - but still failing

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,6 @@ class Ability
 
   def initialize(user)
       user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
+      can :manage, :all
   end
 end
